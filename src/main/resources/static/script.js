1fa8c3dc7eca245ce3df559a7ab9f8fe592c6fb3
Przemyslaw <salamonikp@gmail.com>
2020-03-19 16:08:34 +0100

    Add custom info control

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -27,9 +27,67 @@ function style(feature) {
         fillColor: getColor(color),
         weight: 2,
         opacity: 1,
-        color: 'black',
+        color: '#000000',
         dashArray: '1',
-        fillOpacity: 0.7
+        fillOpacity: 0.5
     };
 }
-L.geoJson(window.statesData, {style: style}).addTo(map);
+var geojson = L.geoJson(window.statesData, {style: style}).addTo(map);
+
+function highlightFeature(event) {
+    var layer = event.target;
+
+    layer.setStyle({
+        weight: 4,
+        color: '#808080',
+        dashArray: '',
+        fillOpacity: 0.5
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+
+    info.update(layer.feature.properties);
+}
+
+function resetHighlight(event) {
+    geojson.resetStyle(event.target);
+    info.update();
+}
+
+function zoomToFeature(event) {
+    map.fitBounds(event.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+geojson = L.geoJson(statesData, {
+    style: style,
+    onEachFeature: onEachFeature
+}).addTo(map);
+
+
+var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function (props) {
+    this._div.innerHTML = '<h4>COVID-19 Confirmed cases</h4>' +  (props ?
+        '<b>' + props.NAME + '</b><br />' + data[props.NAME]+ ' cases'
+        : 'Hover over a state');
+};
+
+info.addTo(map);
+
