b6974b07242f9ae4d4dfdb835bfa26a02f6d3199
Przemyslaw <salamonikp@gmail.com>
2020-03-20 16:24:06 +0100

    Add README.md file

---
+++ README.md
@@ -0,0 +1,1 @@
+Web application visualising COVID-19 confirmed cases for states of US.
--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -84,17 +84,18 @@ info.update = function (props) {
         : 'Hover over a state');
 };
 info.addTo(map);
-
 var legend = L.control({position: 'bottomright'});
 var grades = [0, 10, 20, 50, 100, 200, 500, 1000];
 
 legend.onAdd = function (map) {
 
     var div = L.DomUtil.create('div', 'legend');
+
+
     for (var i = 0; i < grades.length; i++) {
         div.innerHTML +=
             '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+            grades[i] + (grades[i + 1] ? ' &ndash; ' + grades[i + 1] + '<br>' : '+');
     }
     return div;
 };
